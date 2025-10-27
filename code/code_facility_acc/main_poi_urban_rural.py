df09c76ca8f7667a6a35bc4d6735ed1c4d224ae7
Ruichen <148143771+Ruichen-giser@users.noreply.github.com>
2025-10-27 14:28:05 +0800

    Add files via upload

---
+++ code/code_facility_acc/main_poi_urban_rural.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  2 09:29:11 2025
+
+@author: de
+"""
+
+from tools import global_config, overlay_tool_thread_poi
+import os
+import time
+import geopandas as gpd
+import pandas as pd
+pd.set_option('display.max_columns', None) 
+from pathlib import Path
+
+
+if __name__ == "__main__":
+    
+    # main.py directory
+    current_dir = Path(__file__).resolve().parent
+    
+    start_time = time.perf_counter()
+    
+    # Suffixes for the POI column to define the table header names.
+    poi_count_type = ["poi_count"]
+
+    # Carger type
+    cs_type = ['evse_num', 'ev_dc_fast_num','ev_level1_evse_num', 'ev_level2_evse_num']
+    
+    # U.S. boundary: rural-->rural_countries; urban-->urban_countries
+    usa_map_path = current_dir.parent.parent / 'data' /'US-map'/ 'rural_countries.geojson'
+    #usa_map_path = current_dir.parent.parent / 'data' /'US-map'/ 'urban_countries.geojson' # urban boundaries
+    
+    usa_grid_1km_path = current_dir.parent.parent / 'data' /'US-map'/ 'merged_usa_grid_1km.gpkg' 
+    global_config.usa_map_gdf = gpd.read_file(usa_map_path, engine='pyogrio', columns=['NAME_1', 'NAME_2', 'GID_2', 'geometry'])
+    print('reading USA boundary data finished!')
+
+    # Loop through years 2014 to 2024
+    for year in range(2014,2025):
+    #for year in [2024]:# code if you want to run a single year
+        print (f"{year}: main poi")
+        # poi dataset
+        poi_file = current_dir.parent.parent / 'data' /'US-poi-2014-2024' / f'data_{year}_geoparquet.parquet'
+        stations_file = current_dir.parent.parent / 'data' /'US-EV-Station-2014-2024'/f'alt_fuel_stations_historical_day (Dec 31 {year}).geojson'
+
+        # Acc and equity calculation
+        d0=1 # catchment size (km) 
+        POITYPE = "POI"
+        result = overlay_tool_thread_poi.overlay_tiff_poi(d0, poi_file, usa_grid_1km_path, stations_file, poi_count_type, None, cs_type,POITYPE)
+    
+        # efine the output directory based on distance threshold
+        if d0 == 0.25:
+            output_dir = f"./results/POI_resutls/{year}_poi_urban_{d0 * 1000}m"
+        elif d0 == 1:
+            output_dir = f"./results/POI_resutls/{year}_poi_urban_{d0}km"
+
+        os.makedirs(output_dir, exist_ok=True)
+        
+        # save accessibility_summaries
+        for supply_type, summary in result["accessibility_summaries"].items():
+            filepath = os.path.join(output_dir, f"{year}_accessibility_summary_{supply_type}.csv")
+            summary.to_csv(filepath, index=False)
+            print(f"{supply_type} saved to: {filepath}")
+        
+        # save gini_summaries
+        for supply_type, gini_summary in result["gini_summaries"].items():
+            filepath = os.path.join(output_dir, f"{year}_gini_summary_{supply_type}.csv")
+            gini_summary.to_csv(filepath, index=False)
+            print(f"{year}_{supply_type} saved to: {filepath}")
+
+        end_time = time.perf_counter()
+        elapsed_time_minutes = (end_time - start_time) / 60
+    
+        print(f"Elapsed time in minutes: {elapsed_time_minutes:.2f} minutes")
+
